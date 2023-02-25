577db60393416bd7d6015b0680277fe0009a5c00
cb-tennakoon <cbandara613@gmail.com>
2023-02-25 19:57:28 +0530

    first commit

---
+++ src/main/java/com/example/simpleemployeemanagementsystemwebapplication/SimpleEmployeeManagementSystemWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpleemployeemanagementsystemwebapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleEmployeeManagementSystemWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleEmployeeManagementSystemWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simpleemployeemanagementsystemwebapplication/SimpleEmployeeManagementSystemWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpleemployeemanagementsystemwebapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleEmployeeManagementSystemWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
